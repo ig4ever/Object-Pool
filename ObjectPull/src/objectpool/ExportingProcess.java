5694a98ec75b7ceaea6c83daa5563a31e280833f
ig4ever <rahmatig4ever@gmail.com>
2016-08-09 23:10:17 +0700

    Add files via upload

---
+++ ObjectPull/src/objectpool/ExportingProcess.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objectpool;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class ExportingProcess {
+
+    private long processNo;
+
+    public ExportingProcess(long processNo) {
+        this.processNo = processNo;
+        // do some  expensive calls / tasks here in future  
+        // .........  
+        System.out.println("Object with process no. " + processNo + " was created");
+    }
+
+    public long getProcessNo() {
+        return processNo;
+    }
+}
